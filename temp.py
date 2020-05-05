b7532b313d433b76ca7b4664bd4e7b440d026fdd
Matteo <matteo.vettosi@trustonic.com>
2020-05-05 01:44:43 +0100

    added time to test log

--- temp.py
+++ temp.py
@@ -1,11 +1,12 @@
 import os
 import time
+from datetime import datetime
 
 API_TOKEN = os.getenv('API_TOKEN')
 
 
 while True:
     print(f'API_TOKEN is {API_TOKEN}')
+    print(f'And time is {datetime.now().strftime("%H:%M:%S")}')
     time.sleep(1)
     pass
-
