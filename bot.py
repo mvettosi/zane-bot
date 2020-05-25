0955b0f7ca03fc48e6de584d4b0cf591d42cf54e
mvettosi <matteo.vettosi@gmail.com>
2020-05-25 18:17:36 +0100

    Back to warning

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ COGS_DIR = 'cogs'
 
 
 logging.basicConfig(level=logging.INFO)
-logging.getLogger("discord").setLevel(logging.DEBUG)
+logging.getLogger("discord").setLevel(logging.WARNING)
 help_command = CustomHelpCommand(no_category='Others', commands_heading='commands:')
 bot = commands.Bot(command_prefix=config.BOT_PREFIX, description=config.BOT_DESC, help_command=help_command)
 
