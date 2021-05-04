5077e44d2eab82ccc926ef2fbc0a911eaa39aa0e
Matteo Vettosi <matteo.vettosi@gmail.com>
2021-05-04 23:57:25 +0100

    Update config.py
    Added dl annotator config

--- modules/config.py
+++ modules/config.py
@@ -7,6 +7,7 @@ BOT_DESC = os.getenv('BOT_DESCRIPTION')
 BOT_COLOR = int('3a6778', 16)
 DB_CONNECTION_URL = os.getenv('DB_CONNECTION_URL')
 DB_NAME = os.getenv('DB_NAME')
+CARD_ANNOTATOR_URL = os.getenv('CARD_ANNOTATOR_URL')
 
 with open('data/emotes.json') as emotes_file:
     EMOTES = json.load(emotes_file)
