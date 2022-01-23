54ff0e41fea296c7307acd0f9c7abeb43e2259f4
Matteo Vettosi <matteo.vettosi@gmail.com>
2022-01-23 20:53:48 +0000

    removed bot description and prefix from env

--- modules/config.py
+++ modules/config.py
@@ -1,9 +1,9 @@
 import os
 import json
 
-BOT_PREFIX = os.getenv('BOT_PREFIX')
+BOT_PREFIX = "."
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-BOT_DESC = os.getenv('BOT_DESCRIPTION')
+BOT_DESC = "A bot to retrieve various YuGiOh information"
 BOT_COLOR = int('3a6778', 16)
 DB_CONNECTION_URL = os.getenv('DB_CONNECTION_URL')
 DB_NAME = os.getenv('DB_NAME')
